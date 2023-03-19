9372cf635b069d9bbbd6fe5acf953a571e8493d4
Mhmd-Taher <95319818+Mhmd-Taher@users.noreply.github.com>
2023-03-20 01:59:20 +0200

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      // theme: ThemeData.dark(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: "Poppins",
+      ),
       home: const NotesView(),
     );
   }
---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: Color(0xffffcd79),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              "Flutter Tips",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                "Build Your Career With Tharwat Samy",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              "May 21, 2022",
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

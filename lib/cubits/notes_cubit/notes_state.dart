63a8e9a10911f76c4c88cffa4ed33be361285eb3
Mhmd-Taher <95319818+Mhmd-Taher@users.noreply.github.com>
2023-03-24 05:44:11 +0200

    display notes part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -5,16 +5,3 @@ abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
 
-class NotesLoading extends NotesState {}
-
-class NotesSuccess extends NotesState {
-  final List<NoteModel> note;
-
-  NotesSuccess(this.note);
-}
-
-class NotesFailure extends NotesState {
-  final String errMessage;
-
-  NotesFailure(this.errMessage);
-}

33898a304e1913f965fef0f44c648dbc903a1022
Mhmd-Taher <95319818+Mhmd-Taher@users.noreply.github.com>
2023-03-25 16:13:57 +0200

    add colors list to edit note view

--- lib/widgets/edit_ note_view_body.dart
+++ lib/widgets/edit_ note_view_body.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/colors_list_view.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:notes_app/widgets/edit_note_colors_list_view.dart';
 
 class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({super.key, required this.note});
@@ -17,44 +21,52 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
   String? title, content;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          CustomAppBar(
-            onPressed: () {
-              widget.note.title = title ?? widget.note.title;
-              widget.note.subTitle = content ?? widget.note.subTitle;
-              widget.note.save();
-              BlocProvider.of<NotesCubit>(context).fetchAllNotes();
-              Navigator.pop(context);
-            },
-            title: "Edit Note",
-            icon: Icons.check,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomTextField(
-            onChanged: (value) {
-              title = value;
-            },
-            hint: widget.note.title,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: widget.note.subTitle,
-            maxLines: 5,
-            onChanged: (value) {
-              content = value;
-            },
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            CustomAppBar(
+              onPressed: () {
+                widget.note.title = title ?? widget.note.title;
+                widget.note.subTitle = content ?? widget.note.subTitle;
+                widget.note.save();
+                BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                Navigator.pop(context);
+              },
+              title: "Edit Note",
+              icon: Icons.check,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomTextField(
+              onChanged: (value) {
+                title = value;
+              },
+              hint: widget.note.title,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: widget.note.subTitle,
+              maxLines: 5,
+              onChanged: (value) {
+                content = value;
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            EditNoteColorsList(
+              note: widget.note,
+            ),
+          ],
+        ),
       ),
     );
   }
